76e18039832988b77f50d47af669afecfd107f7b
홍석주 <hsj202201@gmail.com>
2025-04-10 12:20:20 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Logue</h1>
+      </header>
+      <main className="app-main">
+        <div className="content-container">
+          {/* 여기에 페이지 내용이 들어갑니다 */}
+        </div>
+      </main>
+      <footer className="app-footer">
+        {/* 푸터 내용 */}
+      </footer>
+    </div>
+  )
+}
+
+export default App
