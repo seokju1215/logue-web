dac8a4c9899e7dee1d2b1559f75d654afa88fce7
홍석주 <hsj202201@gmail.com>
2025-07-26 16:32:13 +0900

    fix. mainpage 글 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
           <p>당신을 나타내는 단 하나의 책장</p>
         </div>
         <div className="info-section">
-          <p>Flutter 앱에서 생성된 프로필 링크로 접속하세요.</p>
+          <p>앱에서 생성된 프로필 링크로 접속하세요.</p>
           <p>예: /u/username</p>
         </div>
       </div>
